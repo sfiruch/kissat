a4fb8e46e6632baaae1850e99e908d41ef12c1d2
Simon Felix <de@iru.ch>
2022-06-23 15:02:50 +0200

    update build.h for sc2022-light

--- src/build.h
+++ src/build.h
@@ -2,7 +2,7 @@
 #define STRINGIZE_NX(a) #a
 #define STRINGIZE(a) STRINGIZE_NX(a)
 
-#define VERSION "sc2021"
+#define VERSION "sc2022-light"
 #define COMPILER "Microsoft (R) C/C++ Optimizing Compiler " STRINGIZE(_MSC_FULL_VER)
-#define ID "baef4609163f542dc08f43aef02ce8da0581a2b5"
+#define ID "0fbf419553c0144342a7cd0e070de25a2c1ec915"
 #define BUILD __DATE__ " " __TIME__
