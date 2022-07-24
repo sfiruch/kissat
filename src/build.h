f3c9615b011b76fb389d5538de31c95b0abed2fb
Simon Felix <de@iru.ch>
2022-07-24 21:56:29 +0200

    release 3.0.0

--- src/build.h
+++ src/build.h
@@ -2,7 +2,7 @@
 #define STRINGIZE_NX(a) #a
 #define STRINGIZE(a) STRINGIZE_NX(a)
 
-#define VERSION "sc2022-light"
+#define VERSION "3.0.0"
 #define COMPILER "Microsoft (R) C/C++ Optimizing Compiler " STRINGIZE(_MSC_FULL_VER)
-#define ID "0fbf419553c0144342a7cd0e070de25a2c1ec915"
+#define ID "2d57d5ee42d74b76a442201ad1c70318d5c1d000"
 #define BUILD __DATE__ " " __TIME__
